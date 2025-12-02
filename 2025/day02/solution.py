57ece2b223d1af614d43c56672c67e70dca7167c
Vedran Blazenka <blazenka.vedran@gmail.com>
2025-12-02 07:33:07 +0100

    feat(2025): solve day2, part1 - not optimized

--- 2025/day02/solution.py
+++ 2025/day02/solution.py
@@ -1,8 +1,17 @@
 """Advent of Code 2025 - Day 2"""
 
+# I'll optimize the solution :D
 def part1(data: str) -> int | str | None:
-    """Solve part 1."""
-    pass
+    sum = 0
+    for line in data.split(','):
+        lb, rb = line.split("-")
+        for i in range(int(lb), int(rb) + 1):
+            if (len(str(i))%2 == 0):
+                l, r = str(i)[:len(str(i))//2], str(i)[len(str(i))//2:]
+                if (l==r):
+                    sum += int(f"{l}{r}")
+    return sum
+
 
 def part2(data: str) -> int | str | None:
     """Solve part 2."""
@@ -14,4 +23,4 @@ if __name__ == "__main__":
         data = f.read().strip()
 
     print(f"Part 1: {part1(data)}")
-    print(f"Part 2: {part2(data)}")
+    # print(f"Part 2: {part2(data)}")
